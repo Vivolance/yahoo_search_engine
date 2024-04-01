8c6b3cc3f91594fa17f0cfbb8363dbb2f6c35aa3
Elson Chan <elsonccjgmail.com>
2024-04-01 23:16:36 +0800

    🐛 Make search result nullable

--- src/models/search_results.py
+++ src/models/search_results.py
@@ -8,11 +8,11 @@ class SearchResults(BaseModel):
     search_id: str
     user_id: str
     search_term: str
-    result: str
+    result: str | None
     created_at: str
 
     @staticmethod
-    def create(user_id: str, search_term: str, result: str) -> "SearchResults":
+    def create(user_id: str, search_term: str, result: str | None) -> "SearchResults":
         return SearchResults(
             search_id=str(uuid.uuid4()),
             user_id=user_id,
